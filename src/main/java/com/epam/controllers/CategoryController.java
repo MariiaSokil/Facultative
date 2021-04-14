e2371a88dd6ab0d7b911dcf4e9673346794426e9
LnAgnrGW <masherka1979@gmail.com>
2021-04-14 20:46:19 +0200

    [migration to spring]- CRUD methods for category

---
+++ src/main/java/com/epam/controllers/CategoryController.java
@@ -0,0 +1,54 @@
+package com.epam.controllers;
+
+import com.epam.model.Category;
+import com.epam.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServlet;
+import java.util.List;
+
+/**
+ * CategoryServlet.
+ * @author M.Sokil
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class CategoryController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    private final CategoryService categoryService;
+
+   /* @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), categoryService.findAll());
+    }*/
+//---------------------------------------------------------------------
+    @GetMapping("/categories")
+    public List<Category> findAll() {
+        return categoryService.findAll();
+    }
+
+    @GetMapping("/categories/{id}")
+    public Category findById(@PathVariable Long id) {
+        return categoryService.findById(id);
+
+    }
+
+    @PostMapping("/categories")
+    public Category createNew(@RequestBody Category newCategory) {
+        return categoryService.save(newCategory);
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public void deleteById(@PathVariable Long id) {
+        categoryService.deleteById(id);
+    }
+
+    @PutMapping("/categories/{id}")
+    public Category updateCategory(@PathVariable Long id, Category category) {
+        return categoryService.updateCategory(id, category);
+    }
+}
