5deeb237df5987f66f385f9576d91de2dca45233
LnAgnrGW <masherka1979@gmail.com>
2021-04-17 10:14:36 +0200

    [migration to spring]- in course and user added annotation of validation

--- src/main/java/com/epam/model/Course.java
+++ src/main/java/com/epam/model/Course.java
@@ -7,11 +7,16 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+
 /**
  * Course model.
+ *
  * @author M.Sokil
  */
 @Builder
@@ -22,22 +27,44 @@ import java.util.Set;
 @Table(name = "courses")
 public class Course {
     @Id
+    @NotNull
     private Long id;
-    @Column(nullable=false)
+
+
+    @NotBlank(message = "Title is mandatory")
+    @Size(max = 50)
+    @NotNull
+    @Column(unique = true)
     private String title;
 
+    @NotNull
+    @NotBlank(message = "Category is mandatory")
     @ManyToOne
-    @JoinColumn(name = "category")
+    @JoinColumn(name = "category",unique = true)
     private Category category;
 
+    @NotNull
+    @Column(name = "duration")
     private int duration;
-    @Column(name="start_date")
+
+    @Column(name = "start_date")
+    @NotNull
     private LocalDate startDate;
+
+    @NotNull
+    @Column(name = "price")
     private Integer price;
+
     @JsonIgnore
-    @ManyToMany(mappedBy="courses")
+    @ManyToMany(mappedBy = "courses")
     private Set<User> students = new HashSet<>();
-  //  private User teacher;
+
+    //  private User teacher;
+    @NotNull
+    @Column(name = "status")
     private Status status;
+
+    @NotNull
+    @Column(name = "enrollment")
     private int enrollment;
 }
--- src/main/java/com/epam/model/User.java
+++ src/main/java/com/epam/model/User.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,8 @@ import java.util.List;
  * @author M.Sokil
  *
  */
+
+
 @Data
 @ToString(exclude = {"courses"})
 @EqualsAndHashCode(exclude = {"courses"})
@@ -26,26 +29,49 @@ import java.util.List;
 @Table(name = "users")
 public class User {
     @Id
-//    @GeneratedValue((strategy = GenerationType.SEQUENCE))
-//    @SequenceGenerator()
+    @NotNull
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+//    @SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq")
     @Column(name="user_id")
     private Long id;
+
+    @NotNull
+    @NotBlank(message = "First_name is mandatory")
+    @Size(max=20)
     @Column(name="first_name")
     private String firstName;
+
+    @NotNull
+    @NotBlank(message = "Last_name is mandatory")
+    @Size(max=20)
     @Column(name="last_name")
     private String lastName;
+
     @Column(name="role_id")
     private Role role;
+
+    @NotNull
+    @NotBlank(message = "Login is mandatory")
+    @Size(max=30)
+    @Column(name = "login",unique = true)
+    @Email
     private String login;
+
+    @NotNull
+    @Size(max=30)
+    @NotBlank(message = "Password is mandatory")
     private String password;
+
     @JsonIgnore
     @ManyToMany
     @JoinTable(name = "users_courses",
             joinColumns = { @JoinColumn(name = "userid") },
             inverseJoinColumns = { @JoinColumn(name = "course_id") })
     private List<Course> courses = new ArrayList<>();
+
     @Column(name="is_student")
     private boolean isStudent;
+
     @Column(name="is_blocked")
     private boolean isBlocked;
 }
