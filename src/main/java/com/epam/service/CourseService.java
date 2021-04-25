8bda18816b807f761e4fabbd0d896d9ac42b3f06
Mariia <masherka1979@gmail.com>
2021-04-25 18:39:05 +0200

    [migration_to_spring]-CourseServiceTest added

--- src/main/java/com/epam/service/CourseService.java
+++ src/main/java/com/epam/service/CourseService.java
@@ -1,8 +1,6 @@
 package com.epam.service;
 
-import com.epam.dao.CourseDao;
 import com.epam.model.Course;
-import com.epam.model.User;
 import com.epam.repository.CourseRepository;
 import com.epam.validator.BasicInfo;
 import lombok.RequiredArgsConstructor;
@@ -22,8 +20,6 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CourseService {
-
-    private  CourseDao courseDao;
 
     private final CourseRepository courseRepository;
 
@@ -63,55 +59,5 @@ public class CourseService {
 
     public Page<Course> findAll(Pageable pageRequest) {
         return courseRepository.findAll(pageRequest);
-    }
-//-------------------------------------------------------------------------------
-    /**
-     * Return List of courses.
-     * If flag eagerStudents true, each course contains List of students.
-     * If flag false, List of student is not completed
-     * @param eagerStudents  boolean.
-     * @return list of courses.
-     */
-    public List<Course> findAll(boolean eagerStudents) {
-        return courseDao.findAll(eagerStudents);
-    }
-
-    /**
-     * Update course.
-     * If bindUser true, then add User to course.
-     * If bindUser false, then delete User from course.
-     * @param course Course.
-     * @param bindUser  boolean.
-     */
-    public void updateCourse(@Validated(BasicInfo.class) Course course, boolean bindUser){
-        courseDao.updateCourse(course, bindUser);
-    }
-
-    /**
-     * Update course.
-     * @param course Course.
-     */
-    public void updateCourse(@Validated(BasicInfo.class) Course course){
-        courseDao.updateCourse(course);
-    }
-
-    /**
-     * Return List of courses by Student id.
-     * @param studentId  Long.
-     * @return list of courses.
-     */
-    public List<Course> findAllByStudentId(Long studentId) {
-        return courseDao.findAllByStudentId(studentId);
-    }
-
-    /**
-     * Delete course.
-     * If removeAssociations true, then delete all Users from course.
-     * If removeAssociations false, just delete course.
-     * @param courseId Long.
-     * @param removeAssociations  boolean.
-     */
-    public void deleteCourse(Long courseId, boolean removeAssociations)  {
-        courseDao.deleteCourse(courseId, removeAssociations);
     }
 }
