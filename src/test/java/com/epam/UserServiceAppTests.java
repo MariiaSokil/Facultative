11cffba9f1ba4404cb63243083057b4723c3204c
LnAgnrGW <masherka1979@gmail.com>
2021-05-31 11:58:47 +0200

    [migration_to_spring]- removing unnecessary

--- src/test/java/com/epam/UserServiceAppTests.java
+++ src/test/java/com/epam/UserServiceAppTests.java
@@ -1,13 +1,21 @@
 package com.epam;
 
 
+import com.epam.controllers.type.UserType;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class UserServiceAppTests {
 
     @LocalServerPort
@@ -16,8 +24,13 @@ public class UserServiceAppTests {
     @Autowired
     TestRestTemplate testRestTemplate;
 
-   /* @Test
-    void findById(){
-        testRestTemplate.getForEntity("http://localhost:" + port +"$/users/{id}");
-    }*/
+    @Value("${users.id}")
+    private Long id;
+
+    @Test
+    public void findById(){
+        UserType userType =  testRestTemplate
+                .getForObject("http://localhost:" + port +"/users"+id, UserType.class);
+        assertEquals(userType.getUserDTO().getId(), id);
+    }
 }
