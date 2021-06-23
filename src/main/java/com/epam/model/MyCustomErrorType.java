c71d3b9f8bcbe072d52d5d6163dfd4bd480d4429
mashe <masherka1979@gmail.com>
2021-06-23 12:22:12 +0200

    [microservices] - legacy code removed

--- src/main/java/com/epam/model/MyCustomErrorType.java
+++
@@ -1,8 +0,0 @@
-package com.epam.model;
-
-public enum MyCustomErrorType {
-    VALIDATION_ERROR_TYPE,
-    PROCESSING_ERROR_TYPE,
-    DATABASE_ERROR_TYPE,
-    FATAL_ERROR_TYPE
-}
--- src/test/java/com/epam/controllers/it/UserControllerTestIT.java
+++
@@ -1,4 +0,0 @@
-package com.epam.controllers.it;
-
-public class UserControllerTestIT {
-}
